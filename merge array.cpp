5022b691264ff884fcb78947958b57853a393dd0
VanshGarg355 <vanshgarg355@gmail.com>
2026-09-14 09:32:49 +0530

    done

--- merge array.cpp
+++ merge array.cpp
@@ -75,6 +75,78 @@ void UnionArray(vector<int> a, vector<int> b)
     cout << endl;
 }
 
+// intersection
+void IntersectionArray(vector<int> a, vector<int> b)
+{
+    int i = 0;
+    int j = 0;
+
+    while (i < a.size() && j < b.size())
+    {
+        if (a[i] < b[j])
+        {
+            i++;
+        }
+        else if (a[i] > b[j])
+        {
+            j++;
+        }
+        else
+        {
+            cout << a[i] << " ";
+            i++;
+            j++;
+        }
+    }
+
+    while (i < a.size())
+    {
+        cout << a[i] << " ";
+        i++;
+    }
+
+    while (j < b.size())
+    {
+        cout << b[j] << " ";
+        j++;
+    }
+
+    cout << endl;
+}
+
+// A - B
+void DiffArray(vector<int> a, vector<int> b)
+{
+    int i = 0;
+    int j = 0;
+
+    while (i < a.size() && j < b.size())
+    {
+        if (a[i] < b[j])
+        {
+            cout << a[i] << " ";
+            i++;
+        }
+        else if (b[j] < a[i])
+        {
+            j++;
+        }
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+
+    while (i < a.size())
+    {
+        cout << a[i] << " ";
+        i++;
+    }
+
+    cout << endl;
+}
+
 int main()
 {
     vector<int> a = {1, 2, 3, 5, 9, 34};
@@ -85,6 +157,12 @@ int main()
 
     cout << "Union of array :" << endl;
     UnionArray(a, b);
+
+    cout << "Intersection of array :" << endl;
+    IntersectionArray(a, b);
+
+    cout << "Difference A-B :" << endl;
+    DiffArray(a, b);
 
     return 0;
 }
